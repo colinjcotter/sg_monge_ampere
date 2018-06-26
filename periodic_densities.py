64f5ac769a39aa10d56ad5466588cf2e86d4e294
cvr12 <cvr12@ic.ac.uk>
2018-06-26 13:32:10 +0100

    added averages and moments functions

--- periodic_densities.py
+++ periodic_densities.py
@@ -1,8 +1,13 @@
+import matplotlib
+matplotlib.use('Agg')
+
 import MongeAmpere as ma
 import numpy as np
 import scipy as sp
 import pylab
 import pdb
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
 
 class Periodic_density_in_x (ma.ma.Density_2):
     def __init__(self, X, f, T, bbox): 
@@ -85,9 +90,65 @@ class Periodic_density_in_x (ma.ma.Density_2):
 
         # rescale the moments to get centroids
         Y /= np.tile(m,(2,1)).T
-        #Y = self.to_fundamental_domain(Y);
+        Y = self.to_fundamental_domain(Y);
         return (Y,m)
 
+    def moments(self,Y,w=None):
+        if w is None:
+            w = np.zeros(Y.shape[0]);
+        N = Y.shape[0];
+        Y0 = self.to_fundamental_domain(Y)
+
+        # create copies of the points, so as to cover the neighborhood
+        # of the fundamental domain.
+        x = self.u[0]
+        y = self.u[1]
+        v = np.array([[0,0], [x,0], [-x,0]]);
+        Yf = np.zeros((3*N,2))
+        wf = np.hstack((w,w,w));
+        for i in xrange(0,3):
+            Nb = N*i; Ne = N*(i+1)
+            Yf[Nb:Ne,:] = Y0 + np.tile(v[i,:],(N,1))
+
+        # sum the moments and masses of each "piece" of the Voronoi
+        # cells
+        [mf,Yf,If] = ma.ma.moments_2(self, Yf, wf)
+        return mf,Yf,If
+
+    #FIX
+    def averages(self,Y,w=None):
+        if w is None:
+            w = np.zeros(Y.shape[0]);
+        N = Y.shape[0];
+        Y0 = self.to_fundamental_domain(Y)
+
+        # create copies of the points, so as to cover the neighborhood
+        # of the fundamental domain.
+        x = self.u[0]
+        y = self.u[1]
+        v = np.array([[0,0], [x,0], [-x,0]]);
+        Yf = np.zeros((3*N,2))
+        wf = np.hstack((w,w,w));
+        for i in xrange(0,3):
+            Nb = N*i; Ne = N*(i+1)
+            Yf[Nb:Ne,:] = Y0 + np.tile(v[i,:],(N,1))
+
+        # sum the moments and masses of each "piece" of the Voronoi
+        # cells
+        [mf,Yf0,If] = ma.ma.moments_2(self, Yf, wf)
+
+        Ya = np.zeros((N,2)); #average of X-x, z 
+        m = np.zeros(N); #total area of cell
+        for i in xrange(0,3):
+            Nb = N*i; Ne = N*(i+1);
+            m += mf[Nb:Ne]
+            ww = np.tile(mf[Nb:Ne],(2,1)).T
+            Ya[:,1] += Yf[Nb:Ne,1] - ww * np.tile(v[i,1],(N,1))
+            Ya[:,0] += Yf0[Nb:Ne,0] - Yf[Nb:Ne,0]*mf[Nb:Ne,0]
+
+        Ya /= np.tile(m,(2,1)).T
+        Ya = self.to_fundamental_domain(Y);
+        return Ya 
 
 # generate density
 def sample_rectangle(bbox):
@@ -105,9 +166,8 @@ def periodicinx_draw_laguerre_cells_2(pxdens,Y,w):
     # x direction
     N = Y.shape[0]
     Y0 = pxdens.to_fundamental_domain(Y)
-    x = pxdens.u[0]
-    y = pxdens.u[1]
-    v = np.array([[0,0], [x,0], [-x,0]])
+    x0 = pxdens.u[0]
+    v = np.array([[0,0], [x0,0], [-x0,0]])
     Yf = np.zeros((3*N,2))
     wf = np.hstack((w,w,w))
     for i in xrange(0,3):
@@ -126,5 +186,7 @@ def periodicinx_draw_laguerre_cells_2(pxdens,Y,w):
     y[3*a] = E[:,1]
     y[3*a+1] = E[:,3]
     y[3*a+2] = nan
-    plt.plot(Y[:,0],Y[:,1],'.')
-    plt.plot(x,y,color=[1,0,0],linewidth=1,aa=True)
+    return x,y
+    #plt.plot(Y[:,0],Y[:,1],'.')
+    #plt.plot(x,y,color=[1,0,0],linewidth=1,aa=True)
+    #plt.savefig('periodic_plot.png')
