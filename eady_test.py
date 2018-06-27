2ed1a6b7ba0d1573d327fa6b6707b38e60b8cc51
cvr12 <cvr12@ic.ac.uk>
2018-06-27 14:01:43 +0100

    Test code for setting up initial condition for eady problem

---
+++ eady_test.py
@@ -0,0 +1,33 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+from periodic_densities import Periodic_density_in_x, sample_rectangle
+import MongeAmpere as ma
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+from eady_initial import initialise_points, eady_OT, forward_euler_sg
+
+N = 20
+H = 1.e4
+L = 1.e6
+
+bbox = np.array([-L, 0., L, H])
+Xdens = sample_rectangle(bbox)
+f0 = np.ones(4)/(H*2*L)
+rho = np.zeros(Xdens.shape[0])
+T = ma.delaunay_2(Xdens,rho)
+dens = Periodic_density_in_x(Xdens,f0,T,bbox)
+
+[Y, thetap] = initialise_points(N, bbox, RegularMesh = False)
+
+w = eady_OT(Y, bbox, dens, verbose = True)
+
+[Y, m] = dens.lloyd(Y,w)
+
+sc = plt.scatter(Y[:,0],Y[:,1],c=thetap,cmap="plasma")
+plt.colorbar(sc)
+plt.savefig('final.png')
+#plt.show()
+
