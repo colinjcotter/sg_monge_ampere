3b356621284eb75ba92dce7fdf9cb735ded82d9e
cvr12 <cvr12@ic.ac.uk>
2018-07-03 12:49:12 +0100

    Testing timestep

--- eady_test.py
+++ eady_test.py
@@ -22,25 +22,28 @@ rho = np.zeros(Xdens.shape[0])
 T = ma.delaunay_2(Xdens,rho)
 dens = Periodic_density_in_x(Xdens,f0,T,bbox)
 
-[Y, thetap] = initialise_points(N, bbox, RegularMesh = True)
-
+[Y, thetap] = initialise_points(N, bbox, RegularMesh = False)
+print(thetap.shape)
 thetap.tofile('thetap.txt',sep=" ",format="%s")
 
 if not timestep:
     w = eady_OT(Y, bbox, dens, verbose = True)
-
     [Y, m] = dens.lloyd(Y,w)
-
+    print(Y.shape)
+    Y.tofile('e_data.txt',sep=" ",format="%s")
     sc = plt.scatter(Y[:,0],Y[:,1],c=thetap,cmap="plasma")
     plt.colorbar(sc)
     plt.savefig('final.png')
     #plt.show()
 
 else:
-    tf = 60*60*5
+    tf = 60*60*24*2
     Y = forward_euler_sg(Y, dens, tf, bbox)
-    Y.tofile('eady_data.txt',sep=" ",format="%s")
-    sc = plt.scatter(Y[:,0],Y[:,1],c=thetap,cmap="plasma")
-    plt.colorbar(sc)
-    plt.savefig('eady_image.png')
+    #print(Y[:,0].min())
+    #print(Y[:,0].max())
+    print(Y.shape)
+    Y.tofile('e_data.txt',sep=" ",format="%s")
+    #sc = plt.scatter(Y[:,0],Y[:,1],c=thetap,cmap="plasma")
+    #plt.colorbar(sc)
+    #plt.savefig('eady_image.png')
     #plt.show()
