e565380fffdb9d0b56579279a7e08893d1028894
cvr12 <cvr12@ic.ac.uk>
2018-07-23 13:16:12 +0100

    Changed to work for Eady Problem

--- plotting_tests.py
+++ plotting_tests.py
@@ -2,72 +2,68 @@ import matplotlib
 matplotlib.use('Agg')
 
 import numpy as np
-from periodic_densities import Periodic_density_in_x, sample_rectangle,periodicinx_draw_laguerre_cells_2
+import periodic_densities as pdx
 import MongeAmpere as ma
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 from PIL import Image
 
-# source: uniform measure on the square with sidelength 1
-bbox = [0.,0.,1.,1.]
-X = np.array([[0, 0],
-              [1, 0],
-              [1, 1],
-              [0, 1]], dtype=float);
-#T = ma.delaunay_2(X,np.zeros(4));
-#mu = np.ones(4);
-#dens = ma.Density_2(X,mu,T);
-#print "mass=%g"%dens.mass()
+N = 10000
+L = 1.e6
+H = 1.e4
 
-Xdens = sample_rectangle(bbox)
-f0 = np.ones(4)
+bbox = np.array([-L, 0., L, H])
+Xdens = pdx.sample_rectangle(bbox)
+f0 = np.ones(4)/(2*L*H)
 rho = np.zeros(Xdens.shape[0])
 T = ma.delaunay_2(Xdens,rho)
-dens = Periodic_density_in_x(Xdens,f0,T,bbox)
-print "mass=%g"%dens.mass()
+dens = pdx.Periodic_density_in_x(Xdens,f0,T,bbox)
 
-# target is a random set of points, with random weights
-N = 6;
-Y = np.random.rand(N,2)/2;
-nu = 10+np.random.rand(N);
-nu = (dens.mass() / np.sum(nu)) * nu;
+Y = np.random.rand(N,2)
+xY[:,0] = Y[:,0]*2*L - L
+Y[:,1] *= H
+#print(Y)
 
-# target is a random set of points, with random weights
-N = 6;
-Y = np.random.rand(N,2);
-nu = np.ones(N);
-nu = (dens.mass() / np.sum(nu)) * nu;
+print(Y.shape)
+nu = np.ones(Y[:,0].size);
+nu = (dens.mass() / np.sum(nu)) * nu
 
-print "mass(nu) = %f" % sum(nu)
-print "mass(mu) = %f" % dens.mass()
- 
-w = ma.optimal_transport_2(dens,Y,nu)
+w = ma.optimal_transport_2(dens, Y,nu , verbose = True)
 
-C = np.random.rand(N,1)
+#C = Y[:,1].reshape((N,1))
+#print(C)
+Nsq = 2.5e-5
+g = 10.
+f = 1.e-4
+theta0 = 300
+C = 3e-6
+B = 0.255
+C = Nsq*theta0*Y[:,1]/g + B*np.sin(np.pi*(Y[:,0] + Y[:,1]))
+C = C.reshape((N,1))
+C = (C - np.ones((C.size,1))*min(C))/(max(C)-min(C))
 
-N = Y.shape[0]
-Y0 = dens.to_fundamental_domain(Y)
-x0 = dens.u[0]
-v = np.array([[0,0], [x0,0], [-x0,0]])
-Yf = np.zeros((3*N,2))
-wf = np.hstack((w,w,w))
-Cf = np.vstack((C,C,C))
-for i in xrange(0,3):
-    Nb = N*i
-    Ne = N*(i+1)
-    Yf[Nb:Ne,:] = Y0 + np.tile(v[i,:],(N,1))
+print(w.shape)
+print(C.shape)
 
-img = ma.laguerre_diagram_to_image(dens,Yf,wf,Cf,bbox,1000,1000)
-print(type(img))
-print(img.shape)
-img.tofile("laguerre_diagram.txt",sep=" ",format="%s")
-#print(type(img))
-#img.save("img1.png","png")
+#Y = np.fromfile('eady_data_5.txt',sep = " ")
+#l = int(Y.size/2)
+#Y = Y.reshape((l,2))
+#print(Y)
+#w = np.fromfile('eady_weights_5.txt',sep = " ")
+#print(w.shape)
+#C = np.fromfile('thetap_5.txt',sep= " ")
+#C = C.reshape((l,1))
+#print(C.shape)
+#C = (C - np.ones((C.size,1))*min(C))/(max(C)-min(C))
 
-[E,x,y] = periodicinx_draw_laguerre_cells_2(dens,Y,w)
-Y.tofile('points_data.txt',sep=" ",format="%s")
-x.tofile('x_data.txt',sep=" ",format="%s")
-y.tofile('y_data.txt',sep=" ",format="%s")
+
+img = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C,bbox,100,10000)
+img.tofile("eady_laguerre_diagram.txt",sep=" ",format="%s")
+
+[E,x,y] = pdx.periodicinx_draw_laguerre_cells_2(dens,Y,w)
+#Y.tofile('points_data.txt',sep=" ",format="%s")
+#x.tofile('x_data.txt',sep=" ",format="%s")
+#y.tofile('y_data.txt',sep=" ",format="%s")
 plt.plot(Y[:,0],Y[:,1],'.')
 plt.plot(x,y,color=[1,0,0],linewidth=1,aa=True)
 plt.savefig('periodic_plot.png')
