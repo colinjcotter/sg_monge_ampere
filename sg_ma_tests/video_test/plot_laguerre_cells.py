23db7ceabb91e04bb33ff9c2bdb73cbaffa31bfb
cvr12 <cvr12@ic.ac.uk>
2018-08-03 11:39:01 +0100

    updates

--- sg_ma_tests/video_test/plot_laguerre_cells.py
+++ sg_ma_tests/video_test/plot_laguerre_cells.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
 
+import os
 import numpy as np
 import periodic_densities as pdx
 import MongeAmpere as ma
@@ -23,11 +24,12 @@ C = C.reshape((l,1))
 C = (C - np.ones((C.size,1))*min(C))/(max(C)-min(C))
 C = np.hstack((C,0.*C,1-C))
 
-for n in range(1,385):
+for n in range(380,390,10):
     print(n)
     Y = np.fromfile('points_results/Gpoints_'+str(n)+'.txt',sep = " ")
     w = np.fromfile('weights_results/weights_'+str(n)+'.txt',sep = " ")
     Y = Y.reshape((l,2))
     img = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C,bbox,1000,1000)
     img.tofile('eady_laguerre_diagram'+str(n)+'.txt',sep=" ",format="%s")
-
+    os.remove('points_results/Gpoints_'+str(n)+'.txt')
+    os.remove('weights_results/weights_'+str(n)+'.txt')
