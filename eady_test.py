eb5b1d303845652107faa35e6b31ae709c56d8e6
cvr12 <cvr12@ic.ac.uk>
2018-08-31 20:52:15 +0100

    streamlined process for creating and saving data in directories

--- eady_test.py
+++ eady_test.py
@@ -11,18 +11,20 @@ import os
 from eady_initial import initialise_points, eady_OT, forward_euler_sg, heun_sg
 
 add_data = True
+Heun = True
 days = 25
 
 #initialise number of grid points and final time
-N = 50
+N = 40
 tf = 60*60*24*days
+tstepsize = 1800.
 
 #initialise parameters from SG Eady model
 H = 1.e4
 L = 1.e6
 g = 10.
 f = 1.e-4
-theta0 = 300
+theta0 = 300.
 
 #create new directory to store results
 newdir = "Results_"+str(days)+"D_"+str(N)+"N_euler"
@@ -37,16 +39,27 @@ T = ma.delaunay_2(Xdens,rho)
 dens = Periodic_density_in_x(Xdens,f0,T,bbox)
 
 #initialise points in geostrophic space
-[Y, thetap] = initialise_points(N, bbox, RegularMesh = True)
+[Y, thetap] = initialise_points(N, bbox, RegularMesh = False)
 Y = dens.to_fundamental_domain(Y)
 Y.tofile(newdir+'/points_0.txt',sep=" ",format="%s")
 thetap.tofile(newdir+'/thetap_init.txt',sep=" ",format="%s")
 
-#timestep using forward euler scheme
-[Y, w, t] = forward_euler_sg(Y, dens, tf, bbox, add_data = True)
-Y.tofile(newdir+'/Gpoints_final.txt',sep=" ",format="%s")
-w.tofile(newdir+'/weights_final.txt',sep=" ",format="%s")
-t.tofile(newdir+'/time.txt',sep=" ",format="%s")
-thetap = Y[:,1]*f*f*theta0/g
-thetap.tofile(newdir+'/thetap_final.txt',sep=" ",format="%s")
+if Heun:
+    #timestep using Heun's Method
+    [Y, w, t] = heun_sg(Y, dens, tf, bbox, add_data = True)
+    Y.tofile(newdir+'/Gpoints_final.txt',sep=" ",format="%s")
+    w.tofile(newdir+'/weights_final.txt',sep=" ",format="%s")
+    t.tofile(newdir+'/time.txt',sep=" ",format="%s")
+    thetap = Y[:,1]*f*f*theta0/g
+    thetap.tofile(newdir+'/thetap_final.txt',sep=" ",format="%s")
+
+else:
+    #timestep using forward euler scheme
+    [Y, w, t] = forward_euler_sg(Y, dens, tf, bbox, add_data = True)
+    Y.tofile(newdir+'/Gpoints_final.txt',sep=" ",format="%s")
+    w.tofile(newdir+'/weights_final.txt',sep=" ",format="%s")
+    t.tofile(newdir+'/time.txt',sep=" ",format="%s")
+    thetap = Y[:,1]*f*f*theta0/g
+    thetap.tofile(newdir+'/thetap_final.txt',sep=" ",format="%s")
+
     
