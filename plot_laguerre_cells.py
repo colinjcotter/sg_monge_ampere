b2f621996c318a366a649f8cac376485bbe99067
cvr12 <cvr12@ic.ac.uk>
2018-09-07 16:23:56 +0100

    Script to plot laguerre cells with diagonal pixel fix

--- plot_laguerre_cells.py
+++ plot_laguerre_cells.py
@@ -12,42 +12,81 @@ import matplotlib.tri as tri
 import periodic_densities as pdx
 from PIL import Image
 
-H = 1.e4
+H = 1.e4          #domain parameters
 L = 1.e6
+Nsq = 2.5e-5
+g = 10.
+f = 1.e-4
+theta0 = 300.
 
-colour = True
+Heun = True      # use Heun's method for time integration
+days = 25         # number of days 
+N = 40            # grid points
+tstepsize = 1800. # stepsize
 
-bbox = np.array([-L, 0., L, H])
-Xdens = pdx.sample_rectangle(bbox)
-f0 = np.ones(4)/2/L/H
-rho = np.zeros(Xdens.shape[0])
-T = ma.delaunay_2(Xdens,rho)
-dens = pdx.Periodic_density_in_x(Xdens,f0,T,bbox)
-
-Y = np.fromfile('Gpoints.txt',sep = " ")
-w = np.fromfile('weights.txt',sep=" ")
-C1 = np.fromfile('thetap_init.txt',sep= " ")
-C2 = np.fromfile('thetap_final.txt',sep= " ")
-
-l = int(Y.size/2)
-Y = Y.reshape((l,2))
-C1 = C1.reshape((l,1))
-C1 = (C1 - np.ones((C1.size,1))*min(C1))/(max(C1)-min(C1))
-C2 = C2.reshape((l,1))
-C2 = (C2 - np.ones((C2.size,1))*min(C2))/(max(C2)-min(C2))
-
-if colour:
-    C1 = np.hstack((C1,0.*C1,1-C1))
-    img1 = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C1,bbox,1000,1000)
-    img1.tofile("laguerre_diagram_init.txt",sep=" ",format="%s")
-    C2 = np.hstack((C2,0.*C2,1-C2))
-    img2 = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C2,bbox,1000,1000)
-    img2.tofile("laguerre_diagram_final.txt",sep=" ",format="%s")
-    
+#initialise image directory and set path of results directory
+if Heun:
+    resultsdir = "/scratchcomp04/cvr12/B25_results/Results_"+str(days)+"D_"+str(N)+"N_"+str(int(tstepsize))+"_heun"
+    imgdir = resultsdir+'/laguerre_diagrams'
+    #os.mkdir(imgdir)
 else:
-    img1 = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C1,bbox,1000,1000)
-    img1.tofile("laguerre_diagram_init.txt",sep=" ",format="%s")
-    img2 = pdx.periodic_laguerre_diagram_to_image(dens,Y,w,C2,bbox,1000,1000)
-    img2.tofile("laguerre_diagram_final.txt",sep=" ",format="%s")
+    resultsdir = "/scratchcomp04/cvr12/B25_results/Results_"+str(days)+"D_"+str(N)+"N_"+str(int(tstepsize))+"_euler"
+    imgdir = resultsdir+'/laguerre_diagrams'
+    os.mkdir(imgdir)
+
+colour = False
+
+Tdomain = np.array([[-L ,0.],[-L, H],[L,H],[L,0.]])
+Tpoints = np.random.rand(20,2)
+Tpoints[:,0] = Tpoints[:,0]*2*L - L
+Tpoints[:,1] = Tpoints[:,1]*H
+Tri = np.vstack((Tdomain,Tpoints))
+
+#initialise periodic density
+bbox = np.array([-L, 0., L, H])
+T = ma.delaunay_2(Tri)
+f0 = np.ones(24)/2/L/H
+
+dens = pdx.Periodic_density_in_x(Tri,f0,T,bbox)
+
+
+#M = int(np.ceil((60*60*24*days)/tstepsize))
+M = 1
+n = 0
+count = 0
+
+while n <= M:
+    Y = np.fromfile(resultsdir+'/points_results_'+str(int(tstepsize))+'/points_'+str(int(n))+'.txt',sep = " ")
+
+    l = int(Y.size/2)
+    Y = Y.reshape((l,2))
+
+    w = np.fromfile(resultsdir+'/weights_results_'+str(int(tstepsize))+'/weights_'+str(int(n))+'.txt',sep=" ")
+
+    #set colour array and reshape for rasterization
+    thetap = f*f*theta0*Y[:,1]/g
+    thetap = thetap.reshape((l,1))
+    thetap = (thetap - min(thetap))/(max(thetap)-min(thetap))
+
+    A = pdx.periodicinx_rasterization(dens,Y,w,thetap,bbox,1500,1500)
+
+    x,y = pdx.periodicinx_draw_laguerre_cells_2(dens,Y,w)
     
+    # plt.figure(figsize=(6,4))
+    # plt.pcolormesh(A[0].T,cmap = "plasma")
+    # plt.colorbar()
+    # plt.axis('off')
+    # plt.savefig(imgdir+'/laguerre_diagram_'+str(int(count))+'.png')
+
+    # plt.figure(figsize=(6,4))
+    # plt.plot(x,y,color=[1,0,0],linewidth=1,aa=True)
+    # plt.savefig(imgdir+'/laguerre_tesselation_'+str(int(count))+'.png')
+
+    plt.figure(figsize=(6,4))
+    plt.plot(Y[:,0],Y[:,1],'.')
+    plt.xticks(np.array([-1000000,-500000,0,500000,1000000]))
+    plt.savefig(imgdir+'/Gpoints_'+str(int(count))+'.png')
+
+    n += 24
+    count += 1
 
